ed319138e59aceae10888a98c50c76c25899ab91
David Marinov <73773220+DavidMarinov22@users.noreply.github.com>
2023-03-12 01:32:34 +0200

    Added new connection and admin page without logic

--- TestStartDP/Data/WeatherForecast.cs
+++ TestStartDP/Data/WeatherForecast.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace TestStartDP.Data
 {
     public class WeatherForecast
@@ -10,6 +8,6 @@ namespace TestStartDP.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
